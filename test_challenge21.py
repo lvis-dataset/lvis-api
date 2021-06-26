ad989b9d382a3d64f1624129e74bd197f9cb784e
Achal Dave <achalddave@live.com>
2021-06-26 17:55:33 -0400

    Update LVIS challenge sample usage

--- test_challenge21.py
+++ test_challenge21.py
@@ -1,19 +1,10 @@
-import logging
-from lvis import LVIS, LVISResults, LVISEval
+from lvis import LVISEval
 
-# result and val files for 100 randomly sampled images.
-ANNOTATION_PATH = "./data/lvis_val_100.json"
-RESULT_PATH = "./data/lvis_results_100.json"
+if __name__ == "__main__":
+    # result and val files for 100 randomly sampled images.
+    ANNOTATION_PATH = "./data/lvis_val_100.json"
+    RESULT_PATH = "./data/lvis_results_100.json"
 
-ANN_TYPE = 'bbox'
-
-lvis_gt = LVIS(ANNOTATION_PATH)
-lvis_dt = LVISResults(lvis_gt,
-                      RESULT_PATH,
-                      max_dets_per_cat=2,
-                      max_dets_per_im=-1)
-lvis_eval = LVISEval(lvis_gt, lvis_dt, ANN_TYPE)
-lvis_eval.params.max_dets = -1
-lvis_eval.params.max_dets_per_cat = 2
-lvis_eval.run()
-lvis_eval.print_results()
+    lvis_eval = LVISEval(ANNOTATION_PATH, RESULT_PATH, mode="challenge2021")
+    lvis_eval.run()
+    lvis_eval.print_results()
